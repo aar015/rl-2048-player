654e56c8796ca29f8a2d0a22fc7b8cdddf9f8ebe
Avraham Revah <avraham.a.revah@gmail.com>
2019-05-25 15:45:49 -0500

    License

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
 setup(name='rl2048player',
       version='1.0',
       description='Reinforcement Learning Agent capble of playing 2048',
+      long_description=readme(),
       url='https://github.com/aar015/rl-2048-player',
       author='aar015',
       packages=['rl2048player'],
       install_requires=[
-          csv,
-          cv2,
-          imageio,
-          matplotlib,
-          numpy,
-          pickle,
-          random,
-          abc
+          'imageio',
+          'matplotlib',
+          'numpy'
       ],
+      classifiers=[
+          'Programming Language :: Python :: 3.6'
+      ],
+      keywords='2048 RL',
+      include_package_data=True,
       zip_safe=False)
